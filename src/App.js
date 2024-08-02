2e0bfffe7ff516f78f76b612cab2f05620e54776
Naman <namanwinner5@gmail.com>
2024-08-02 22:27:16 +0530

    we finally added a map to the project !!

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
-
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -33,6 +34,9 @@ function App() {
         <Route path='/signup' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/create-listing' element={<CreateListing/>} />
+        <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+        <Route path='/contact/:landlordId' element={<Contact />} />
+        
       </Routes>
       <Navbar />
    </Router>
